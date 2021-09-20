ca2cc17c6ac077fa80c504a89ac585146c710102
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-20 18:18:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ gcc -m32 -c utils.c -o utils.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
 
 gcc -m32 -c char.c -o char.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
 
+gcc -m32 -c start.c -o start.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
+
 #linking the kernel with kernel.o and boot.o files
 ld -m elf_i386 -T linker.ld kernel.o boot.o -o Ardenware.bin -nostdlib
 
