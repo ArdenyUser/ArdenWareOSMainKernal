d0dd56c7bc723dcd7b44a57facb57376579d9f58
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-21 20:04:12 -0400

    Update start.c

--- start.c
+++ start.c
@@ -1,1 +1,49 @@
+#include start.h
 
+int cake_base() 
+{
+  char ch = 0;
+  char cha = 0;
+  char chb = 0;
+  char keycode = 0;
+  do{
+    keycode = get_input_keycode();
+    if(keycode == KEY_ENTER){
+      print_new_line();
+    }else{
+      ch = get_ascii_char(keycode);
+      print_char(ch);
+    }
+    sleep(0x02FFFFFF);
+    }while(ch > 0);
+      do{
+        keycode = get_input_keycode();
+        if(keycode == KEY_ENTER){
+           print_new_line();
+        }else{
+          cha = get_ascii_char(keycode);
+          print_char(cha);
+        }
+        sleep(0x02FFFFFF);
+         }while(ch > 0);
+          do{
+            keycode = get_input_keycode();
+            if(keycode == KEY_ENTER){
+               print_new_line();
+            }else{
+              cha = get_ascii_char(keycode);
+              print_char(cha);
+            }
+            sleep(0x02FFFFFF);
+          }while(ch > 0);
+        }             // FIX BUGS   
+                
+void setup() 
+{
+  print_new_line();
+  init_vga(GREEN, BLACK);
+  print_new_line();
+  print_string("DogeBakery Compiler Version One");
+  print_new_line();
+
+}
